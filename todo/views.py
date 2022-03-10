77314ff5c1bdb5e8d33f08363a5dee6cc34874c6
Ricardo <ricardo.csantos0877@gmail.com>
2022-03-09 23:38:00 -0300

    feat: register page

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+def index(request):
+	if request.user.is_authenticated:
+		pass
+	else:
+		return redirect('login')
 
 def login(request):
 	return render(request, 'login.html')
+
+def register(request):
+	return render(request, 'register.html')
